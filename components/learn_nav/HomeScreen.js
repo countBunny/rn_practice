dc06f67a4c9eb38588dd5de95c4cf54fd48a098a
liepin <zty@liepin.com>
2018-12-13 11:29:37 +0800

    scroll view and touchHightLight touchNative widget

--- components/learn_nav/HomeScreen.js
+++ components/learn_nav/HomeScreen.js
@@ -1,74 +1,86 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import React, {Component} from 'react';
+import {View, StyleSheet, Button, ScrollView} from 'react-native';
 import TouchExample from "../learn_state/TouchScreen";
 
 export default class HomeScreen extends Component {
     static navigationOptions = {
         title: "Welcome",
     };
+
     render() {
         return (
-            <View style={styles.container}>
-                <Button
-                    style={styles.btn_items}
-                    title="Go to Jane's profile"
-                    onPress={
-                        () => {
-                            this.props.navigation.navigate('Profile', {
-                                name: 'Jane',
-                            })
+            <ScrollView>
+                <View style={styles.container}>
+                    <Button
+                        style={styles.btn_items}
+                        title="Go to Jane's profile"
+                        onPress={
+                            () => {
+                                this.props.navigation.navigate('Profile', {
+                                    name: 'Jane',
+                                })
+                            }
                         }
-                    }
-                />
-                <Button
-                    style={styles.btn_items}
-                    title="got to BlinkPage"
-                    onPress={
-                        () => {
-                            this.props.navigation.navigate('BlinkScreen')
+                    />
+                </View>
+                <View style={styles.container}>
+                    <Button
+                        style={styles.btn_items}
+                        title="got to BlinkPage"
+                        onPress={
+                            () => {
+                                this.props.navigation.navigate('BlinkScreen')
+                            }
                         }
-                    }
-                />
-                <Button
-                    style={styles.btn_items}
-                    title="Go to Film List"
-                    onPress={
-                        () => {
-                            this.props.navigation.navigate('FilmPage')
+                    />
+                </View>
+                <View style={styles.container}>
+                    <Button
+                        style={styles.btn_items}
+                        title="Go to Film List"
+                        onPress={
+                            () => {
+                                this.props.navigation.navigate('FilmPage')
+                            }
                         }
-                    }
 
-                />
-                <Button
-                    style={styles.btn_items}
-                    title="Go to pizza convertor"
-                    onPress={
-                        () => {
-                            this.props.navigation.navigate('PizzaPage')
+                    />
+                </View>
+                <View style={styles.container}>
+                    <Button
+                        style={styles.btn_items}
+                        title="Go to pizza convertor"
+                        onPress={
+                            () => {
+                                this.props.navigation.navigate('PizzaPage')
+                            }
                         }
-                    }
 
-                />
-                <Button
-                    style={styles.btn_items}
-                    title="Go to tap button"
-                    onPress={
-                        () => {
-                            this.props.navigation.navigate('TouchScreen')
+                    />
+                </View>
+                <View style={styles.container}>
+                    <Button
+                        style={styles.btn_items}
+                        title="Go to tap button"
+                        onPress={
+                            () => {
+                                this.props.navigation.navigate('TouchScreen')
+                            }
                         }
-                    }
 
-                />
-            </View>
+                    />
+                </View>
+
+
+            </ScrollView>
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 3,
-        flexDirection:'column',
-        justifyContent: 'space-around',
+        height: 60,
+        justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
--- components/learn_state/TouchScreen.js
+++ components/learn_state/TouchScreen.js
@@ -1,5 +1,17 @@
 import React, {Component} from 'react'
-import {View, Button, StyleSheet, Alert} from 'react-native'
+import {
+    View,
+    Text,
+    Button,
+    TouchableHighlight,
+    TouchableOpacity,
+    TouchableNativeFeedback,
+    TouchableWithoutFeedback,
+    StyleSheet,
+    ScrollView,
+    Alert,
+    Platform
+} from 'react-native'
 
 export default class TouchExample extends Component {
     static navigationOptions = {
@@ -12,7 +24,7 @@ export default class TouchExample extends Component {
     }
 
     render() {
-        return (<View style={this.styles.container}>
+        return (<ScrollView>
             <View style={this.styles.buttonContainer}>
                 <Button title="Press Me" onPress={this._tapButton}/>
             </View>
@@ -23,11 +35,47 @@ export default class TouchExample extends Component {
                 <Button title="This looks great!" onPress={this._tapButton}/>
                 <Button title="OK!" color="#841584" onPress={this._tapButton}/>
             </View>
-        </View>);
+            <TouchableHighlight style={this.styles.buttonContainer} onPress={this._onPress} underlayColor="white">
+                <View style={this.styles.button}>
+                    <Text style={this.styles.buttonText}>TouchableHighlight</Text>
+                </View>
+            </TouchableHighlight>
+            <TouchableOpacity style={this.styles.buttonContainer} onPress={this._onPress}>
+                <View style={this.styles.button}>
+                    <Text style={this.styles.buttonText}>TouchableOpacity</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableNativeFeedback style={this.styles.buttonContainerNative} onPress={this._onPress}
+                                     background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
+                <View style={this.styles.button}>
+                    <Text style={this.styles.buttonText}>TouchableNativeFeedback</Text>
+                </View>
+            </TouchableNativeFeedback>
+            <TouchableWithoutFeedback style={this.styles.buttonContainerNative} onPress={this._onPress}>
+                <View style={this.styles.button}>
+                    <Text style={this.styles.buttonText}>TouchableWithoutFeedback</Text>
+                </View>
+            </TouchableWithoutFeedback>
+            <TouchableHighlight style={this.styles.buttonContainer} onPress={this._onPress}
+                                      onLongPress={this._longPress}
+                                      underlayColor="white">
+                <View style={this.styles.button}>
+                    <Text style={this.styles.buttonText}>Touchable With Long Press</Text>
+                </View>
+            </TouchableHighlight>
+        </ScrollView>);
     }
 
     _tapButton() {
         Alert.alert("you tapped the button!")
+    }
+
+    _onPress() {
+        Alert.alert("you press on the button!")
+    }
+
+    _longPress() {
+        Alert.alert("long pressed here!")
     }
 
     const
@@ -36,13 +84,29 @@ export default class TouchExample extends Component {
             flex: 1,
             justifyContent: 'center',
         },
-        buttonContainer:{
-            margin: 20
+        buttonContainer: {
+            margin: 20,
+            justifyContent: 'center'
         },
-        alternativeLayoutButtonContainer:{
+        buttonContainerNative: {
+            margin: 20,
+            justifyContent: 'center',
+        },
+        button: {
+            marginBottom: 30,
+            width: 260,
+            alignItems: 'center',
+            backgroundColor: '#2196f3'
+        },
+        alternativeLayoutButtonContainer: {
             margin: 20,
             flexDirection: 'row',
             justifyContent: 'space-between'
-        }
+        },
+        buttonText: {
+            padding: 20,
+            color: 'white'
+        },
+
     })
 };
